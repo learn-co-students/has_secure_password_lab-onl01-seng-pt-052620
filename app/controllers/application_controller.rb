83f063f9f61316b1de3d2ca134c8fd5f3d432ba2
pete3249 <katelynrpeterson16@gmail.com>
2020-10-28 19:12:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find(session[:user_id])
+    User.find_by(id: session[:user_id])
   end 
-  
+
+  def logged_in?
+    current_user.id != nil
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
     def new
-        @user = User.new
     end 
 
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             render :welcome
         else
             flash[:notice] = "Name and password cannot be blank"
